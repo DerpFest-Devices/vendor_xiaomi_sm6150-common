be494750abf6b677949094cf5d482d63c9543657
adi8900 <adrianszymanski242@gmail.com>
2021-05-03 11:34:44 +0200

    sm6150-common: Restore updatable gpu support
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -692,6 +692,7 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
+    com.qualcomm.qti.gpudrivers.sm6150.api30 \
     PowerOffAlarm \
     DeviceInfo \
     QtiTelephonyService \
