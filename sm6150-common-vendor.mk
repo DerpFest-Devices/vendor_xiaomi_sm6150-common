e7e4c73a3cf8837fbcf43dde1052842efcee5fe8
adi8900 <adrianszymanski242@gmail.com>
2024-01-17 21:22:35 +0100

    sm6150-common: Update system blobs from LA.QSSI.14.0.r1-11100-qssi.0
    * ims depends on: https://github.com/DerpFest-AOSP/vendor_codeaurora_telephony/commit/c6c474c4ee331a1c0eb1b54f7f9d515a7a7af21e
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -665,9 +665,9 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne.constants@1.0 \
     com.quicinc.cne.constants@2.0 \
     com.quicinc.cne.constants@2.1 \
-    SoterService \
     CneApp \
     IWlanService \
+    SoterService \
     TimeService \
     DeviceInfo \
     ImsRcsService \
