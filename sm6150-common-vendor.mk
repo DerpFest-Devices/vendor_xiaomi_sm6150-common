a102b671a268255b4a4c4156d4242f85187a828f
adi8900 <adrianszymanski242@gmail.com>
2025-02-11 17:51:59 +0100

    sm6150-common: Update vendor from 13700 msmsteppe
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -432,6 +432,7 @@ PRODUCT_PACKAGES += \
     dpmapi \
     qcrilhook \
     qti-telephony-common \
+    android.hardware.keymaster@4.1-service-default-qti.xml \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
     vendor.qti.gnss@4.0-service.xml \
     adpl \
